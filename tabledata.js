c2c72b9c615d81b1dc24024831b7df4cb01f6725
skippy <rcthedev@gmail.com>
2020-01-22 20:21:09 -0700

    added some waitlist.js and tabledata.js

---
+++ tabledata.js
@@ -0,0 +1,11 @@
+let tableArray = [
+    {
+        customerName: "Bob",
+        customerEmail: "bob@bob.com",
+        customerID: "bob69",
+        phoneNumber: "555*555-5555"
+
+    }
+];
+
+module.exports = tableArray;
---
+++ waitlist.js
@@ -0,0 +1,10 @@
+let waitingArray = [
+    {
+        customerName: "Bob",
+        customerEmail: "bob@bob.com",
+        customerID: "bob69",
+        phoneNumber: "555*555-5555"
+    }
+];
+
+module.exports = waitingArray;
